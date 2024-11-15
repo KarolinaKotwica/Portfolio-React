e9ad8c4a0dfe21cdfa293c91c3ae60d5b5574988
Karolina <karolina.kotwica@powercoders.org>
2024-11-15 10:57:17 +0100

    Change text

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -2,7 +2,7 @@ const AboutSection = () => {
     return (
         <section id="about" className="scroll-container">
             <h1>About</h1>
-                <p>Hello, I'm Karolina from Poland, currently living in Switzerland. I've been passionate about web development for quite some time. I love creating attractive, efficient websites that offer excellent user experiences. I'm eager to enhance my skills and am seeking my first job in this field.
+                <p>Hello, I'm Karolina from Poland, currently living in Switzerland. I've been passionate about web development for quite some time. I love creating attractive, efficient websites that offer excellent user experiences.
                 <br /><br />
                 I've self-taught most of what I know. In April 2022, I attended a 13-week Powercoders Bootcamp in Switzerland. This intensive course focused on web development, and each Wednesday, we practiced social and business skills.
                 <br /><br />
@@ -12,7 +12,7 @@ const AboutSection = () => {
                 <ul className="languages">
                     <li><span>Polish:</span> Mother tongue</li>
                     <li><span>English:</span> Intermediate</li>
-                    <li><span>German:</span> Good (and still getting better!)</li>
+                    <li><span>German:</span> Intermediate</li>
                 </ul>
         </section>
     )
